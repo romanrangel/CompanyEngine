1a839609589eded5301a7edcb797a991c9b26f7d
Roman Rangel <romanrangel106549@gmail.com>
2021-01-14 21:21:59 -0800

    deleted instructions README, added basic README file describing project.

--- Develop/app.js
+++ Develop/app.js
@@ -139,7 +139,6 @@ inquirer
 }) .catch( err => {
     console.log(err);
 });
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CompanyEngine
+
+## How it works
+
+This application uses a back end server that stores information that was inputed by a user. The user will be asked a series of prompts that ask for specific information to display on the users employee card.
